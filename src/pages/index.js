8b6f318005fafd4d550e60f93823c75ea7cda973
fernandoors <fernando.ors@outlook.com>
2020-04-04 13:27:32 -0300

    FIX: REMOVE SEO FROM INDEX

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 import BaseView from '../base/BaseView'
 
 const IndexPage = () => {
   return (
     <Layout>
-      {/* <SEO title="Docker Create Image" description="Create your DOCKERFILE using best practices from official documentation and communities suggestions" /> */}
       <BaseView />
     </Layout>
   )
