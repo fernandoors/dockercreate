c7a9161e0b519869c44e965df600e81768f1061c
Bruno Padilha <brpadilha.dev@gmail.com>
2020-04-10 00:46:12 -0300

    create new button suggest template image

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { Link } from "gatsby"
 import { ButtonComponent, Container } from "./styles"
 import { Button, Card } from "antd"
 
@@ -7,18 +7,27 @@ export default function Buttons({ handleTemplate, templateImage }) {
   return (
     <>
       <Container>
-        <Card
-          htmlFor='Images templates references'
-          title="IMAGE TEMPLATE"
-        >
+        <Card htmlFor="Images templates references" title="IMAGE TEMPLATE">
           <ButtonComponent>
             {templateImage.map(image => (
-              <Link aria-label={`Template image to ${image.display}`} key={image.id} to={`/${image.id}`}>
+              <Link
+                aria-label={`Template image to ${image.display}`}
+                key={image.id}
+                to={`/${image.id}`}
+              >
                 <Button shape="round" type="primary">
                   {image.display}
                 </Button>
               </Link>
             ))}
+            <a
+              href="https://github.com/fernandoors/dockercreate/issues/new"
+              target="_blank"
+            >
+              <Button shape="round" type="danger">
+                Suggest Template Image
+              </Button>
+            </a>
           </ButtonComponent>
         </Card>
       </Container>
